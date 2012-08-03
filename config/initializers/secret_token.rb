f193a852f7293adaf31e595ef0c64ef3add17e5d
Maneesh Goel <maneeshgoel@gmail.com>
2012-08-03 11:22:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleChat::Application.config.secret_token = '50c4dacbeedc7a88cb30c1b1b47ee56c8b5fd7e46595501ed8f3febb5e24e31aae7a4901c252748736a0904307635ec31f6d69c193fff7cb13bd8d5e1d250b11'
